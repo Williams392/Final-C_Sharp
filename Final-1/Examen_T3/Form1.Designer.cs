8f236a6bb8f305e82e2e4dac10cba40d6251dd65
Williams392 <williamsvalle392@gmail.com>
2022-11-24 19:09:59 -0500

    1.11

---
+++ Final-1/Examen_T3/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Examen_T3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelContainer = new System.Windows.Forms.Panel();
+            this.BTNsalir = new System.Windows.Forms.Button();
+            this.listbox = new System.Windows.Forms.RichTextBox();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelTitulo = new System.Windows.Forms.Panel();
+            this.btnMinimizar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelContainer.SuspendLayout();
+            this.panelTitulo.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelContainer
+            // 
+            this.panelContainer.BackColor = System.Drawing.Color.LightGray;
+            this.panelContainer.Controls.Add(this.panel1);
+            this.panelContainer.Controls.Add(this.BTNsalir);
+            this.panelContainer.Controls.Add(this.listbox);
+            this.panelContainer.Controls.Add(this.panelTitulo);
+            this.panelContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContainer.Location = new System.Drawing.Point(0, 0);
+            this.panelContainer.Name = "panelContainer";
+            this.panelContainer.Size = new System.Drawing.Size(370, 445);
+            this.panelContainer.TabIndex = 0;
+            this.panelContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.panelContainer_Paint);
+            // 
+            // BTNsalir
+            // 
+            this.BTNsalir.Location = new System.Drawing.Point(141, 399);
+            this.BTNsalir.Name = "BTNsalir";
+            this.BTNsalir.Size = new System.Drawing.Size(75, 23);
+            this.BTNsalir.TabIndex = 10;
+            this.BTNsalir.Text = "Salir";
+            this.BTNsalir.UseVisualStyleBackColor = true;
+            this.BTNsalir.Click += new System.EventHandler(this.BTNsalir_Click);
+            // 
+            // listbox
+            // 
+            this.listbox.Location = new System.Drawing.Point(107, 179);
+            this.listbox.Name = "listbox";
+            this.listbox.Size = new System.Drawing.Size(147, 214);
+            this.listbox.TabIndex = 9;
+            this.listbox.Text = "";
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(25, 66);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(100, 30);
+            this.btnOrdenar.TabIndex = 8;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(171, 68);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(100, 28);
+            this.btnIngresar.TabIndex = 7;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Letra:";
+            // 
+            // panelTitulo
+            // 
+            this.panelTitulo.BackColor = System.Drawing.Color.Black;
+            this.panelTitulo.Controls.Add(this.btnMinimizar);
+            this.panelTitulo.Controls.Add(this.btnCerrar);
+            this.panelTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitulo.Location = new System.Drawing.Point(0, 0);
+            this.panelTitulo.Name = "panelTitulo";
+            this.panelTitulo.Size = new System.Drawing.Size(370, 33);
+            this.panelTitulo.TabIndex = 0;
+            this.panelTitulo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitulo_MouseDown);
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMinimizar.FlatAppearance.BorderSize = 0;
+            this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimizar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinimizar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnMinimizar.Location = new System.Drawing.Point(298, 0);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(36, 33);
+            this.btnMinimizar.TabIndex = 2;
+            this.btnMinimizar.Text = "__";
+            this.btnMinimizar.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnMinimizar.UseVisualStyleBackColor = true;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnCerrar.FlatAppearance.BorderSize = 0;
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCerrar.Location = new System.Drawing.Point(334, 0);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(36, 33);
+            this.btnCerrar.TabIndex = 1;
+            this.btnCerrar.Text = "X";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PowderBlue;
+            this.panel1.Controls.Add(this.btnOrdenar);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btnIngresar);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(32, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(302, 111);
+            this.panel1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 445);
+            this.Controls.Add(this.panelContainer);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panelContainer.ResumeLayout(false);
+            this.panelTitulo.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelContainer;
+        private System.Windows.Forms.Panel panelTitulo;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnMinimizar;
+        private System.Windows.Forms.RichTextBox listbox;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BTNsalir;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Final-2/Examen_T3/Form1.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen_T3
+{
+    public partial class Form1 : Form
+    {
+        // Declararando una lista (Modulo 9)
+        List<Juego> LstJuegos = new List<Juego>();
+
+        int p_indice = 0, p_punts = 0;
+
+
+        // Módulo 10: Manejo de archivos
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // concatenando una galeria de imagenes
+            pictureBox2.ImageLocation = "C:/Users/PC/Desktop/Final-2/img/" + comboBox1.Text + ".png";
+
+        }
+
+
+        // Combobox -> cmbPartida
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPuntos.Enabled = false;
+            // Cargar el Combo
+            cmbPartida.Items.Add("Seleccione: ");
+            cmbPartida.Items.Add("Victoria");
+            cmbPartida.Items.Add("Derrota");
+            cmbPartida.SelectedIndex = 0;
+        }
+
+        // Puntos -> Mostrar en el dataGridView1
+        private void cmbPartida_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                    
+            p_indice = cmbPartida.SelectedIndex;
+
+            switch (p_indice)
+            {
+                case 1:
+                    p_punts = 100;
+                    break;
+                case 2:
+                    p_punts = -20;
+                    break;
+
+            }
+            txtPuntos.Text = p_punts.ToString();
+        }
+
+        // Sumar valores de una columna de un dataGridView1 y mostrar resultado en un (Label) C#
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double p_total = 0;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                                                     // Columna Puntos
+                p_total += Convert.ToDouble(row.Cells["Puntos"].Value);
+            }
+            IBIpuntosObtenidos.Text = p_total.ToString(); // Visualize el RESULTADO en Label
+        }
+
+
+        // Boton REGISTRAR
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+
+            Juego oJuego = new Juego();
+
+            oJuego.usuario = txtUsuario.Text;
+            oJuego.juego = comboBox1.Text;
+            oJuego.dia = cmbDia.Text;
+            oJuego.partida = cmbPartida.Text;
+            oJuego.puntos = txtPuntos.Text;
+
+            LstJuegos.Add(oJuego);
+            // (Modulo_8) Exepciones
+            MessageBox.Show("Se registro con Éxito ✔️");    // -> Mensaje en pantalla
+
+        }
+
+        // Boton Mostrar
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            MostrarLista();
+        }
+        public void MostrarLista()
+        {
+            dataGridView1.Rows.Clear();
+            foreach (Juego item in LstJuegos)
+            {
+                // MessageBox.Show(item.usuario);
+                dataGridView1.Rows.Add(item.getData());
+            }
+        }
+
+
+        // Boton ELIMIAR
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult rppta = MessageBox.Show("¿Desea eliminar?",
+                    "Eliminacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (rppta == DialogResult.Yes)
+                {
+                    dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+                }
+            }
+            catch { }
+        }
+
+
+        // Todo de Abajo -> codigo Exportar Excel
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            exportaraexcel(dataGridView1);
+        }
+        public void exportaraexcel(DataGridView tabla)
+        {
+
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+
+            excel.Application.Workbooks.Add(true);
+
+            int IndiceColumna = 0;
+
+            foreach (DataGridViewColumn col in tabla.Columns)
+            {
+
+                IndiceColumna++;
+
+                excel.Cells[1, IndiceColumna] = col.Name;
+
+            }
+
+            int IndeceFila = 0;
+
+            foreach (DataGridViewRow row in tabla.Rows)
+            {
+
+                IndeceFila++;
+
+                IndiceColumna = 0;
+
+                foreach (DataGridViewColumn col in tabla.Columns)
+                {
+
+                    IndiceColumna++;
+
+                    excel.Cells[IndeceFila + 1, IndiceColumna] = row.Cells[col.Name].Value;
+
+                }
+
+            }
+
+            excel.Visible = true;
+        }
+
+
+        /////////////////////////////////////////////////////////////////////
+        // -> Formulario(el Form1) Redondeado con Bordes / (Todo lo de Abajo)
+
+
+        // Campos
+        private int borderRadius = 30;
+        private int borderSize = 2;
+        private Color borderColor = Color.Black; // cambiar de color_1.1 (del Form)
+
+
+        // Constructor
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Padding = new Padding(borderSize);
+            this.panelTitulo.BackColor = borderColor; // cambiando de color_1.1
+            this.BackColor = borderColor; // Eliminar el parpadeo Blanco
+        }
+
+
+        // Método Mover / Arrastrar Formulario
+        // Arrasta el -> Form
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style |= 0x20000; // <--- Minimize borderless form from taskbar
+                return cp;
+            }
+        }
+
+
+        // Minimizar Formulario sin Bordes desde la Barra de tareas (Form1_MouseDown -> fue llamado desde propiedad)
+        private void Form1_MouseDown(object sender, MouseEventArgs e)  // -> Para mover
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);    // -> mismo para Metros
+        }
+
+        private void panelTitulo_MouseDown(object sender, MouseEventArgs e) // -> Para mover
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);    // -> mismo para Metros
+        }
+
+
+        // Métodos Privados
+        // Crear Ruta Redondeada
+        private GraphicsPath GetRoundedPath(Rectangle rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+
+        // Establecer Región y Borde Redondeado – Formulario
+        private void FormRegionAndBorder(Form form, float radius, Graphics graph, Color borderColor, float borderSize)
+        {
+            if (this.WindowState != FormWindowState.Minimized)
+            {
+                using (GraphicsPath roundPath = GetRoundedPath(form.ClientRectangle, radius))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                using (Matrix transform = new Matrix())
+                {
+                    graph.SmoothingMode = SmoothingMode.AntiAlias;
+                    form.Region = new Region(roundPath);
+                    if (borderSize >= 1)
+                    {
+                        Rectangle rect = form.ClientRectangle;
+                        float scaleX = 1.0F - ((borderSize + 1) / rect.Width);
+                        float scaleY = 1.0F - ((borderSize + 1) / rect.Height);
+
+                        transform.Scale(scaleX, scaleY);
+                        transform.Translate(borderSize / 1.6F, borderSize / 1.6F);
+
+                        graph.Transform = transform;
+                        graph.DrawPath(penBorder, roundPath);  // esto da color al Marco (Color Azul) solo descomillar y se pone en Negro
+                    }
+                }
+            }
+        }
+
+
+        // Establecer Región y Borde Redondeado – Panel Contenedor
+        private void ControlRegionAndBorder(Control control, float radius, Graphics graph, Color borderColor)
+        {
+            using (GraphicsPath roundPath = GetRoundedPath(control.ClientRectangle, radius))
+            using (Pen penBorder = new Pen(borderColor, 1))
+            {
+                graph.SmoothingMode = SmoothingMode.AntiAlias;
+                control.Region = new Region(roundPath);
+                graph.DrawPath(penBorder, roundPath);
+            }
+        }
+
+
+        // Dibujar Rutas – Esquinas del Formulario
+        private void DrawPath(Rectangle rect, Graphics graph, Color color)
+        {
+            using (GraphicsPath roundPath = GetRoundedPath(rect,borderRadius))
+            using (Pen penBorder = new Pen(color, 3))
+            {
+                graph.DrawPath(penBorder,roundPath);
+            }
+        }
+
+
+        // Obtener Colores de los Limites Exteriores del Formulario
+        private struct FormBoundsColors
+        {
+            public Color TopLeftColor;
+            public Color TopRightColor;
+            public Color BottomLeftColor;
+            public Color BottomRightColor;
+        }
+        private FormBoundsColors GetFormBoundsColors()
+        {
+            var fbColor = new FormBoundsColors();
+            using (var bmp = new Bitmap(1, 1))
+            using (Graphics graph = Graphics.FromImage(bmp))
+            {
+                Rectangle rectBmp = new Rectangle(0, 0, 1, 1);
+
+                //Top Left
+                rectBmp.X = this.Bounds.X - 1;
+                rectBmp.Y = this.Bounds.Y;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.TopLeftColor = bmp.GetPixel(0, 0);
+
+                //Top Right
+                rectBmp.X = this.Bounds.Right;
+                rectBmp.Y = this.Bounds.Y;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.TopRightColor = bmp.GetPixel(0, 0);
+
+                //Bottom Left
+                rectBmp.X = this.Bounds.X;
+                rectBmp.Y = this.Bounds.Bottom;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.BottomLeftColor = bmp.GetPixel(0, 0);
+
+                //Bottom Right
+                rectBmp.X = this.Bounds.Right;
+                rectBmp.Y = this.Bounds.Bottom;
+                graph.CopyFromScreen(rectBmp.Location, Point.Empty, rectBmp.Size);
+                fbColor.BottomRightColor = bmp.GetPixel(0, 0);
+            }
+            return fbColor;
+        }
+
+        // Métodos de evento: Establecer Región Redondeado y Dibujar Borde + Suavizado (Formulario)
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            base.OnPaintBackground(e);
+
+            // borde exterior liso
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle rectForm = this.ClientRectangle;
+            int mWidht = rectForm.Width / 2;
+            int mHeight = rectForm.Height / 2;
+            var fbColors = GetFormBoundsColors();
+
+            // arriba a la izquierda -> y el color
+            DrawPath(rectForm,e.Graphics, fbColors.TopLeftColor);
+
+            // parte superior derecha
+            Rectangle rectopRight = new Rectangle(mWidht,rectForm.Y,mWidht,mHeight);
+            DrawPath(rectopRight,e.Graphics, fbColors.TopRightColor);
+
+            // Abajo a la izquierda
+            Rectangle rectBottomLeft = new Rectangle(rectForm.X, rectForm.X + mHeight, mWidht, mHeight);
+            DrawPath(rectBottomLeft, e.Graphics,fbColors.BottomLeftColor);
+
+            // Bottom Right
+            Rectangle rectBottomRight = new Rectangle(mWidht, rectForm.Y + mHeight, mWidht, mHeight);
+            DrawPath(rectBottomRight, e.Graphics, fbColors.BottomRightColor);
+        }
+
+        // llamar desde Propiedad
+        private void Form1_Paint(object sender, PaintEventArgs e)    //Métodos de evento
+        {
+            FormRegionAndBorder(this, borderRadius, e.Graphics, borderColor, borderSize); // -> el formulario
+        }
+        // Establecer Región Redondeado y Dibujar Borde Fino (Panel Contenedor)
+        private void panelContainer_Paint(object sender, PaintEventArgs e)
+        {
+            ControlRegionAndBorder(panelContainer,borderRadius-(borderSize/2), e.Graphics, borderColor);
+        }
+
+
+        // Actualizar Región y Borde
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+
+
+        /////////////////////////////////////////////////////////////////////////////////////////////
+    }
+}
